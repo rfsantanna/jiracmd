85bab7e8f598ffb1783889c7566dc6b26b4a4270
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2022-01-19 02:50:25 -0300

    remove unused file

--- curl_time.sh
+++
@@ -1,3 +0,0 @@
-curl 'https://jirabancobbm.atlassian.net/rest/api/2/issue/PLAT-1449/worklog?adjustEstimate=auto&_r=1642520933290' \
-  --data-raw '{"comment":"","started":"2021-12-01T12:25:00.000+0000","timeSpent":"140m","visibility":null}' \
-  --compressed
