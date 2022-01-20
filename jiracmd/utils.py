96e8a51a99974d88d3bb9342002fbcd21e4d5282
Rafael Sant'Anna <46816722+rfsantanna@users.noreply.github.com>
2022-01-20 19:23:42 -0300

    Remove reversed sort by default

--- jiracmd/utils.py
+++ jiracmd/utils.py
@@ -1,8 +1,8 @@
 from tabulate import tabulate
 
-def output_table(obj, sort_by=None):
+def output_table(obj, sort_by=None, reverse=False):
     if sort_by:
-        obj = sorted(obj, key = lambda x: x[sort_by], reverse=True)
+        obj = sorted(obj, key = lambda x: x[sort_by], reverse=reverse)
     table = tabulate(obj, headers="keys")
     print(table)
     return table
