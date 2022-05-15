afe683a9db25928653c311c9654660c0ed9bfb69
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2022-05-15 18:13:08 -0300

    Add env vars

--- tests/test_jiracmd.py
+++ tests/test_jiracmd.py
@@ -2,20 +2,16 @@ import os
 from click.testing import CliRunner
 from jiracmd.cli import cli
 
-os.environ['JIRA_SERVER'] = "jiratest.atlassian.net"
-os.environ['JIRA_USERNAME'] = "jiratester@jiratest.com"
-os.environ['JIRA_SERVER'] = "alskdjalksdjalksjdalsC"
-
-
 def test_version_flag():
     runner = CliRunner()
     with runner.isolated_filesystem():
         result = runner.invoke(cli, ["--version"])
         assert result.exit_code == 0
-        assert result.output.startswith("cli, version ")
 
 def test_version_output():
     runner = CliRunner()
     with runner.isolated_filesystem():
         result = runner.invoke(cli, ["--version"])
         assert result.output.startswith("cli, version ")
+
+
